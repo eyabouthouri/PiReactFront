5c665341e257cb5044f7a67cf3322bd5f531c554
MariemBenKhlifaa <mariem.benkhlifa@esprit.tn>
2023-05-10 17:54:11 +0200

    ff

--- pi-front/src/helpers/axiosConfig.js
+++ pi-front/src/helpers/axiosConfig.js
@@ -1,2 +1,7 @@
 import axios from "axios";
 axios.defaults.headers.common["Content-Type"] = "application/json";
+if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+    // dev code
+  } else {
+    axios.defaults.baseURL = "https://youthc.onrender.com";
+  }
