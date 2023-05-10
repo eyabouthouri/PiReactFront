75f444f1cfdd72ab1f5d6abf0a058577e2526d57
DorsafCharfeddine <dorsaf.charfeddine@esprit.tn>
2023-05-10 19:23:10 +0100

    config

--- pi-front/src/helpers/axiosConfig.js
+++ pi-front/src/helpers/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 axios.defaults.headers.common["Content-Type"] = "application/json";
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
-    // dev code
-  } else {
-    axios.defaults.baseURL = "https://youthconn.onrender.com";
-  }
+  // dev code
+} else {
+  axios.defaults.baseURL = "https://youthconnectws.onrender.com";
+}
